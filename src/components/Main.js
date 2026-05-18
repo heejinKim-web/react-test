27c92001cd4ec4d5f2958340ab6701e8f8070ad8
Heejin Kim <desireoftruth1126@gmail.com>
2026-05-18 11:24:46 +0900

    react test 페이지 등록

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../style/main.scss";
 
 const Main = () => {
-  return <div>hello react</div>;
+  return (
+    <div>
+      <p>hello react 이제부터 리액트를 써보도록 하겠습니다!</p>
+      <p>hello react 이제부터 리액트를 써보도록 하겠습니다!</p>
+    </div>
+  );
 };
 
 export default Main;
