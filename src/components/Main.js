5e0d5972b6932bd6261c5e82a5c8d7193bf9541c
Heejin Kim <desireoftruth1126@gmail.com>
2026-05-18 11:00:13 +0900

    react router 설치

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>hello react</div>;
+};
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Main";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
