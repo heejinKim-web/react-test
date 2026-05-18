2fafbf763735ca5086af3b4d22d9c5925f627c0f
Heejin Kim <desireoftruth1126@gmail.com>
2026-05-18 16:18:48 +0900

    react main 작업

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,112 @@ import Header from "./common/Header";
 import Footer from "./common/Footer";
 import "../style/main.scss";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+
 const Main = () => {
   return (
-    <div>
-      <p>hello react 이제부터 리액트를 써보도록 하겠습니다!</p>
-      <p>hello react 이제부터 리액트를 써보도록 하겠습니다!</p>
+    <div className="main">
+      <Header />
+      <div className="main_visual">
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={16}
+          freeMode={true}
+          modules={[FreeMode, Autoplay]}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide1.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide2.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide3.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide4.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide5.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide1.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide5.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide4.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/">
+              <img
+                src={`${process.env.PUBLIC_URL}/image/slide3.png`}
+                alt="슬라이드1"
+              />
+            </a>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="container">
+        <div className="main_big_text">
+          <h1>
+            Refined,
+            <br />
+            inspired
+            <br />
+            packaging
+          </h1>
+          <p>고급스러운 디자인과 실용성을 겸비한 패키징 솔루션</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../style/common.scss";
 
-const Header = () => {};
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container header_container">
+        <div className="logo">
+          <Link to="/">CASEMAN</Link>
+        </div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link to="/">CASEMAN</Link>
+            </li>
+            <li>
+              <Link to="/">종이박스</Link>
+            </li>
+            <li>
+              <Link to="/">부직포</Link>
+            </li>
+            <li>
+              <Link to="/">쇼핑백/캐리어</Link>
+            </li>
+            <li>
+              <Link to="/">디자인박스</Link>
+            </li>
+            <li>
+              <Link to="/">선물포장</Link>
+            </li>
+            <li>
+              <Link to="/">케이크박스</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="login">
+          <Link to="/">로그인</Link>
+          <Link to="/">장바구니</Link>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
